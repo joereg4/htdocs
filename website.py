96ab00ae61647cee4db1dc663a4413935052015e
joereg4 <59125710+joereg4@users.noreply.github.com>
2023-05-13 15:01:44 -0500

    Change app.py to website.py

--- website.py
+++ website.py
@@ -17,9 +17,4 @@ def blogfeed():
     return render_template('blogfeed.html', title=feed.feed.title, entries=feed.entries)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
-
-if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
+    app.run(host='0.0.0.0', port=5000)
