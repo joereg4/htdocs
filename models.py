d76cd77105cf3cba64d04c228055484524163efc
Joe Regenstein <59125710+joereg4@users.noreply.github.com>
2023-06-08 21:23:08 -0500

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-#models.py
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Counter(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    page = db.Column(db.String(50), nullable=False)
-    count = db.Column(db.Integer, default=0)
