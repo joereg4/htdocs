d2df7dc44ae2c7c01d085f9c710d03f99681d842
joereg4 <joereg4@protonmail.com>
2023-06-08 20:20:33 -0500

    Add SQLlite

--- website.py
+++ website.py
@@ -1,32 +1,45 @@
-from flask import Flask, render_template, g, request, make_response
+from flask import Flask, render_template
+import os
 import feedparser
-from counter import counter
-import uuid
+from models import db, Counter
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
+migrate = Migrate(app, db)
 
-@app.before_request
-def before_request():
-    visitor_id = request.cookies.get('visitor_id')
-    if not visitor_id:
-        visitor_id = str(uuid.uuid4())
-        g.visitor_id = visitor_id
-        resp = make_response()
-        resp.set_cookie('visitor_id', visitor_id)
+# Configure the SQLite database URI
+database_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+database_file = os.path.join(database_dir, 'count.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + database_file
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the data directory if it doesn't exist
+if not os.path.exists(database_dir):
+    os.makedirs(database_dir)
+
+db.init_app(app)
+
+
+def increment_page_counter(page_name):
+    page_counter = Counter.query.filter_by(page=page_name).first()
+    if page_counter:
+        page_counter.count += 1
     else:
-        g.visitor_id = visitor_id
+        page_counter = Counter(page=page_name, count=1)
+        db.session.add(page_counter)
+    db.session.commit()
 
 
 @app.route('/')
 def home():
-    counter.increment('home', g.visitor_id)
+    increment_page_counter('home')
     return render_template('index.html')
 
 
 @app.route('/blog/')
 def blogfeed():
-    counter.increment('blog', g.visitor_id)
+    increment_page_counter('blog')
     # Parse the RSS feed
     feed = feedparser.parse('http://jregenstein.com/feed')
 
@@ -36,35 +49,32 @@ def blogfeed():
 
 @app.route('/social/')
 def social():
-    counter.increment('social', g.visitor_id)
+    increment_page_counter('social')
     return render_template('social.html')
 
 
 @app.route('/now/')
 def now():
-    counter.increment('now', g.visitor_id)
+    increment_page_counter('now')
     return render_template('now.html')
 
 
 @app.route('/book/')
 def book():
-    counter.increment('book', g.visitor_id)
+    increment_page_counter('book')
     return render_template('book.html')
 
 
 @app.route('/counts/')
 def counts():
-    counts = dict(counter.counter)
+    counts = Counter.query.all()
     return render_template('counts.html', counts=counts)
-
-
-@app.route('/data')
-def data():
-    return counter.get_daily_visits()
 
 
 if __name__ == '__main__':
     app.config['ENV'] = 'development'
     app.config['DEBUG'] = True
     app.config['TESTING'] = True
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000)
